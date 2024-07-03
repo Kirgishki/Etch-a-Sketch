eb8c4e914048ad075b195397962a553328ac3f0f
Zika Linux <115041465+Kirgishki@users.noreply.github.com>
2024-07-03 21:41:39 +0200

    Add button for user to change number of squares - Squares will automatically align

--- main.js
+++ main.js
@@ -2,17 +2,40 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 const btnEnterValue = document.createElement("button");
 
+const containerWidth = 850;
+const containerGap = 2;
+let numOfSquaresPerSide = 64;
+
+container.style.width = containerWidth + "px";
+container.style.gap = containerGap + "px";
+
+
 container.classList.add("container");
 btnEnterValue.innerText = "Change Number of Squares";
 
 body.appendChild(btnEnterValue);
 body.appendChild(container);
 
-for (let i = 1; i <= 256; i++) {
-    const divSquare = document.createElement('div');
-    divSquare.style.width = "20px";
-    divSquare.style.height = "20px";
+fillContainerWithSquares(numOfSquaresPerSide, container, containerWidth, containerGap);
 
-    container.appendChild(divSquare);
+function fillContainerWithSquares(numOfSquaresPerSide, container, widthOfDiv, gap){
+
+    let totalSquares = numOfSquaresPerSide * numOfSquaresPerSide ;
+    let sumGapPerSide = gap * (numOfSquaresPerSide - 1);
+    let squareSideSize = (widthOfDiv - sumGapPerSide) / numOfSquaresPerSide;
+
+    for (let i = 1; i <= totalSquares; i++) {
+        const divSquare = document.createElement('div');
+
+        divSquare.style.width = squareSideSize + "px";
+        divSquare.style.height = squareSideSize + "px";
+
+        container.appendChild(divSquare);
+    }
 }
 
+btnEnterValue.addEventListener('click', ()=>{
+    numOfSquaresPerSide = prompt("Unesite broj kvadrata po redu:");
+    container.replaceChildren();
+    fillContainerWithSquares(numOfSquaresPerSide, container, containerWidth, containerGap);
+});
